b6edfc5ccf9b2204204af37b27e3c42fe1b55d3b
Ramirez Luana <luanaabigail168@gmail.com>
2025-11-12 14:44:59 -0300

    PRIVATE/PUBLIC RUTE

---
+++ src/routes/PrivateRoute.jsx
@@ -0,0 +1,42 @@
+import { useEffect } from "react";
+import { Loading } from "../components/Loading";
+import { Outlet, Navigate } from "react-router-dom";
+export const PrivateRoute = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const checkAut = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/api/profile", {
+        credentials: "include",
+      });
+      if (response.ok) {
+        setIsAuthenticated(true);
+      } else {
+        setIsAuthenticated(false);
+      }
+    } catch (error) {
+      console.log("Error de fetch");
+      setIsAuthenticated(false);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    checkAut();
+  }, []);
+  return isLoading ? (
+    <Loading />
+  ) : isAuthenticated ? (
+    <Outlet />
+  ) : (
+    <Navigate to="/home" />
+  );
+};
+
+// if (isLoading) {
+//   return <Loading />;
+// }
+
+// isLoading ?  cargando...  : (isAuthenticated ? <Outlet /> : <Navigate to="/home"/>)
