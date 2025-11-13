fe9cec11d145a1e9a84eb38b459f0719c42c208d
Ramirez Luana <luanaabigail168@gmail.com>
2025-11-13 14:51:48 -0300

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ export const Footer = () => {
     <footer className="w-full bg-gradient-to-r from-purple-800 to-indigo-900 text-white py-4 mt-auto shadow-inner">
       <div className="max-w-6xl mx-auto px-4 flex flex-col sm:flex-row justify-between items-center text-center sm:text-left">
         <p className="text-sm">
-          © {year} | Desarrollado por{" "}
+          © {year} | Desarrollado por {""}
           <span className="font-semibold">Ramírez Luana Abigail</span>
         </p>
         <p className="text-sm mt-2 sm:mt-0 text-white/80">
